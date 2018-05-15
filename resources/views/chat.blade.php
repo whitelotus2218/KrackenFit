fe71d39cb8fb190d122d81624ac19f5210b8788b
whitelotus2218 <37649289+whitelotus2218@users.noreply.github.com>
2018-05-14 20:30:27 -0500

    Create chat.blade.php

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Document</title>
+   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+    <div class="container">
+         <div class="row">
+                <h1>Chat room</h1>
+         </div>
+    </div>
+
+   <script src="{{ asset('js/app.js')}}"></script>
+</body>
+</html>
