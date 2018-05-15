ff4bc79f63883eb39a03b14adcc3d3956b7d1b93
whitelotus2218 <37649289+whitelotus2218@users.noreply.github.com>
2018-05-15 17:30:36 -0500

    Update chat.blade.php

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,18 +1,58 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
+
+  <meta charset="UTF-8">
+
    <title>Document</title>
+
+   <meta name="csrf-token" content="{{ csrf_token() }}">
+
    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
 
-    <div class="container">
-         <div class="row">
-                <h1>Chat room</h1>
-         </div>
-    </div>
+   <style>
+   	.list-froup{
+   		overflow-y: scroll;
+   		height: 200px 
+   	}
+   </style>
 
-   <script src="{{ asset('js/app.js')}}"></script>
-</body>
-</html>
+ </head>
+ <body>
+ 	<div class="container">
+ 		<div class="row" id="app">
+ 			
+ 			<div class="offset-4 col-4 offset-sm-1 col-sm-10">
+ 				<li class="list-group-item active">Chat room</li>
+
+ 				<div class="badge badge-pill badge-primary">@{{  typing  }}</div>
+                 <ul class="list-group">
+ 				
+ 				    <message
+ 			     v-for="value,index in chat.message"
+ 			     :key=value.index 
+
+                  color= chat.color[index]
+                  :user = chat.user[index]
+ 			     >
+ 			     	@{{ value }}
+ 			        </message>
+
+ 			    </ul>
+              
+
+               <input type="text" class="form-control" placeholder="type your message" v-model='message' @keyup.enter='send'>
+ 			
+ 			</div>
+ 			
+ 		</div>
+
+ 	</div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+ </body>
+
+ </html>
